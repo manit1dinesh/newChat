13a93a9b364ee0e864cb752d1cee804cc033168d
dinesh singh <manit.dinesh@gmail.com>
2022-06-20 23:44:44 +0530

    text chat final

--- application/src/app/chat/chat.page.ts
+++ application/src/app/chat/chat.page.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { io } from 'socket.io-client';
 import { DataService } from '../services/data.service';
 import firebase from 'firebase/compat/app';
@@ -14,7 +14,7 @@ import { Router } from '@angular/router';
 })
 export class ChatPage implements OnInit {
 
-  constructor(private dataService: DataService, private router: Router) { }
+  constructor(private dataService: DataService, private router: Router, private renderer: Renderer2) { }
   socket = io('http://localhost:3000')
   name;
   msg;
@@ -33,13 +33,11 @@ export class ChatPage implements OnInit {
     });
     
     
-    // Recieve messages 
-    this.socket.on('message', (msg) => {
-      console.log(msg);
-      this.msg = msg;
-      this.appendMessage(msg, 'incoming')
-      this.scrollToBottom()
-    })
+   // Recieve messages 
+this.socket.on('message', (msg) => {
+  this.appendMessage(msg, 'incoming')
+  this.scrollToBottom()
+})
     
   }
   
@@ -50,31 +48,32 @@ export class ChatPage implements OnInit {
   }
   
   sendMessage(message) {
-    this.msg = {
-      user: this.name,
-      message: message.trim(),
-      class: 'outgoing'
+    let msg = {
+        user: this.name,
+        message: message.trim()
     }
     // Append 
-    this.appendMessage(this.msg, 'outgoing')
+    this.appendMessage(msg, 'outgoing')
     this.textarea.nativeElement.value = ''
     this.scrollToBottom()
 
     // Send to server 
-    this.socket.emit('message', this.msg)
-    
-  }
+    this.socket.emit('message', msg)
+
+}
   
-  appendMessage(msg, type) {
-    let mainDiv = document.createElement('div')
-    mainDiv.classList.add('message', type)
-    let markup = `
-    <h4>${msg.user}</h4>
-    <p>${msg.message}</p>
-    `
-    mainDiv.innerHTML = markup
-    this.messageArea.nativeElement.appendChild(mainDiv)
-  }
+appendMessage(msg, type) {
+  const mainDiv = this.renderer.createElement('div') as HTMLDivElement;
+  this.renderer.addClass(mainDiv, 'message');
+  this.renderer.addClass(mainDiv, type);
+
+  let markup = `
+      <h4>${msg.user}</h4>
+      <p>${msg.message}</p>
+  `
+  mainDiv.innerHTML = markup
+  this.messageArea.nativeElement.appendChild(mainDiv)
+}
   
   
 
--- application/src/app/tab2/tab2.page.ts
+++ application/src/app/tab2/tab2.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  @ViewChild('check') check: ElementRef
+  constructor(private rendere: Renderer2) { }
 
-  constructor() {}
+  button() {
+    let apandedDiv = document.createElement('div');
+    this.rendere.addClass(this.check.nativeElement, 'appand');
+
+    let markup = "<h1>hello i am check</h1>"
+    apandedDiv.innerHTML = markup
+
+    this.check.nativeElement.appendChild(apandedDiv);
+  }
+
+  ngOnInit(): void {
+  }
 
 }
